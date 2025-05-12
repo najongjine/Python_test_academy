a853ed6d23bf881e2a70f7db31436ae5330a7979
najongjine <najongjin3@hotmail.com>
2025-05-12 11:25:33 +0900

    .

--- 일정/Day4_if문/odd_even_badver.py
+++ 일정/Day4_if문/odd_even_badver.py
@@ -1,16 +1,6 @@
 # input을 사용하면 키보드로부터 입력 받는다.
 number1=input("숫자 정수를 입력해 주세요: ")
 
-# try catch 예외처리기의 특징은 try block 안에 에러가 나도 except
-# block을 실행함
-try:
-    #문자열을 정수로 강제 변환
-    number1=int(number1)
-    None
-except:
-    print(f"정수를 입력 안하셨네요")
-    exit()
-
 if(number1%2 == 0):
     print(f"입력값 {number1} 은 짝수입니다")
 else:
