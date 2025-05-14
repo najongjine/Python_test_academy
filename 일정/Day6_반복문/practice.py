2ad248b6b9c1d5fbd96d75d5b2c443a137f53c0b
najongjine <najongjin3@hotmail.com>
2025-05-14 11:10:05 +0900

    .

--- 일정/Day6_반복문/practice.py
+++ 일정/Day6_반복문/practice.py
@@ -1,6 +1,16 @@
 """
-구구단
-화면에 2 * 1 ~ 9*9 까지 출력하기
-for i in range(9):
-    for i in range(9):
+구구단(특성상 반복함. forloop)
+화면에 2(데이터) *(수학연산) 1(데이터) ~ 9*9 까지 
+출력하기(print 기능)
+for i_1 in range(9):
+    for i_2 in range(9):
+2*1, 2*2, 2*3.... 9*9
+if(i_1 <=1 or i_2 <=0):
+            continue
 """
+for i_1 in range(10):
+    for i_2 in range(10):
+        print(f"{i_1} * {i_2} = {i_1 * i_2}")
+        None
+    None
+
