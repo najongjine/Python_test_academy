e3909128ca97dec40849c26b14168e40166ede79
najongjine <najongjin3@hotmail.com>
2025-05-14 11:22:43 +0900

    .

--- 일정/Day6_반복문/primenum.py
+++ 일정/Day6_반복문/primenum.py
@@ -1,16 +1,20 @@
+
 """
-소수 판별하기
-소수는 1 하고 자기 자신만 나머지(%. mod 연산) 가 0이 된다.
+dummy1=input("정수들 입력하세요")
+정수가 아니면 다시 입력하게끔 하기
+b_int = False
+dummy1 이 데이터, input() 기능, int() 변환기능, 반복문
 """
 
-number1=3
-b_prime=True
-for i in range(number1-1):
-    
-    if number1 % (i+1) == 0 and :
-        print(f"{number1} % {i+1}")    
-        b_prime = False
-if b_prime:
-    print(f"{number1} 는 prime number 입니다")
-else:
-    print(f"{number1} 는 prime number 아닙니다")
+b_repeat=True
+dummy1=""
+while(b_repeat):
+    dummy1=input("정수를 입력하세요: ")
+    try:
+        dummy1=int(dummy1)
+        b_repeat=False
+        None
+    except:
+        b_repeat=True
+        None
+print(f"입력한 값 : {dummy1}")
