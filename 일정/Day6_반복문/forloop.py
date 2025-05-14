b9cfbb2700903fc2f611213fc77cbc63083581ef
najongjine <najongjin3@hotmail.com>
2025-05-14 10:30:43 +0900

    .

--- 일정/Day6_반복문/forloop.py
+++ 일정/Day6_반복문/forloop.py
@@ -16,7 +16,7 @@ while 문을 사용할땐 꼭 종료 조건을 사용해 주어야 한다
 dummy = 0
 while dummy < 100:
     dummy+=1
-    print(f"while 반복문 이에요 {dummy}")
+    #print(f"while 반복문 이에요 {dummy}")
     None
 
 
@@ -24,12 +24,33 @@ while dummy < 100:
 forloop 안에 또 forloop 이 있을수 있다
 """
 for i_1 in range(2):
-    print(f"i_1: {i_1}")
+    #print(f"i_1: {i_1}")
     None
     for i_2 in range(3):
-        print(f"i_1: {i_1}, i_2:{i_2}")
+        #print(f"i_1: {i_1}, i_2:{i_2}")
         None
 
 """
 forloop 는 자동화를 구현하는데 1등급으로 중요하다.
 """
+
+"""
+반복문 멈추기
+"""
+for i_1 in range(2000):
+    if i_1 == 3:
+        #print(f"{i_1} 에서 멈춤")
+        break
+    #print(f"{i_1}")
+    None
+
+"""
+반복문 특정구간 건너뛰기
+"""
+for i_1 in range(3):
+    if i_1 == 2:
+        continue
+    print(f"{i_1}")
+    None
+
+print("프로그램은 계속 실행됨")
