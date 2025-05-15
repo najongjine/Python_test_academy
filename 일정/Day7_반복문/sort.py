3e48b327c86d214c8fe04dfca002e122b00e2823
najongjine <najongjin3@hotmail.com>
2025-05-15 11:46:04 +0900

    .

--- 일정/Day7_반복문/sort.py
+++ 일정/Day7_반복문/sort.py
@@ -4,7 +4,8 @@
 for i in range(len(list1)):
     for j in range(len(list1) - i - 1):
         if list1[j]["가격"] > list1[j + 1]["가격"]:
-"""
+큰 데이터를 그냥 뒤로 밀어버리기
+        """
 
 list1 = [
     {"제품": "뭐뭐1", "가격": 1110},
@@ -12,9 +13,25 @@ list1 = [
     {"제품": "뭐뭐3", "가격": 3005},
     {"제품": "뭐뭐4", "가격": 400},
     {"제품": "뭐뭐5", "가격": 5050},
-    {"제품": "뭐뭐6", "가격": 6800},
-    {"제품": "뭐뭐7", "가격": 700},
-    {"제품": "뭐뭐8", "가격": 80560},
-    {"제품": "뭐뭐9", "가격": 90},
-    {"제품": "뭐뭐10", "가격": 1000}
 ]
+for i in range(len(list1)):
+    for j in range(len(list1)):
+            try:
+                if list1[j]["가격"] > list1[j + 1]["가격"]:
+                    print(f"i:{i}  j:{j}")
+                    #예전값 저장하기
+                    temp = list1[j]
+                    print(f"바뀌기전j : {temp}")
+                    print(f"바뀌기전j+1 : {list1[j+1]}")
+                    # 서로의 위치 바꾸기
+                    list1[j]=list1[j+1]
+                    print(f"j : {list1[j+1]}")
+                    # 서로의 위치 바꾸기
+                    list1[j+1]=temp
+                    print(f"j+1 : {list1[j+1]}")
+            except:
+                None
+for i in list1:
+    #print(i)
+    None
+
