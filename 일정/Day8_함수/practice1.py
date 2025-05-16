38984b9b3c12263306392d46393ac85b48a34f99
najongjine <najongjin3@hotmail.com>
2025-05-16 11:09:41 +0900

    .

--- 일정/Day8_함수/practice1.py
+++ 일정/Day8_함수/practice1.py
@@ -3,3 +3,20 @@
 그 정수가 짝수인지 홀수인지 판별해주는 함수 만들기
 리턴값 자료구조 {"success":... "data"... "message":...}
 """
+def odd_even_decide(num):
+    result={"success":True, "data":"", "message":""}
+    try:
+        num=int(num)
+        if(num%2==0):
+            result["data"]="짝수입니다"
+        else:
+            result["data"]="홀수입니다"
+    except Exception as err:
+        result["success"]=False
+        result["data"]=""
+        result["message"]=err
+    return result
+
+number1=input("정수를 입력하세요 : ")
+result=odd_even_decide( number1)
+print(result)
